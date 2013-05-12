73064de8c7f74a4fbb237f1b8688c81af0d2e1b1
Will <will@tiny-rhino.co.uk>
2013-05-12 19:38:52 +0100

    Changed animations

--- dev2/charge.php
+++ dev2/charge.php
@@ -6,6 +6,8 @@
         <h2 class="featurette-heading">The Charge<span class="muted">It'll blow your mind.</span></h2>
         <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
          
+
+         
       <div  class="row" style="clear:both;">
       
       
@@ -36,12 +38,20 @@
       </div><!-- /.row -->
       
       
+              <div id="moreChargeNav" class="spaceForNav" ></div>
+      
+       <div  class="row">
+      	<div id="morechange1" class="moreCharge">
+        hello
+        </div>
+      </div>
+      
             <div  class="row">
         <div class="span4 " >
                 <img class="img-circle chargeIcons" alt="140x140" style="width: 140px; height: 140px;" src="http://www.smarterwebcompany.co.uk/thesmarterwebcompany-co-uk/_img/homepage_services/ecom-design.png">
           <h2>Ecommerce Site</h2>
           <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
-           <p id="nav"><a class="btn" href="#moreChargeNav" onclick="TRStore()">View details &raquo;</a></p>
+           <p id="nav"><a class="btn" href="#moreChargeNav2" onclick="TRStore()">View details &raquo;</a></p>
         </div><!-- /.span4 -->
         
         
@@ -50,7 +60,7 @@
                 <img class="img-circle chargeIcons" alt="140x140" style="width: 140px; height: 140px;" src="http://www.smarterwebcompany.co.uk/thesmarterwebcompany-co-uk/_img/homepage_services/mobile-websites.png">
           <h2>Moblie Site</h2>
           <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-           <p id="nav"><a class="btn" href="#moreChargeNav" onclick="TRMobile()">View details &raquo;</a></p>
+           <p id="nav"><a class="btn" href="#moreChargeNav2" onclick="TRMobile()">View details &raquo;</a></p>
         </div><!-- /.span4 -->
         
         
@@ -59,15 +69,15 @@
                 <img class="img-circle chargeIcons" alt="140x140" style="width: 140px; height: 140px;" src="http://www.smarterwebcompany.co.uk/thesmarterwebcompany-co-uk/_img/homepage_services/seo-service.png">
           <h2>Some other service because 5 is shit number</h2>
           <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-           <p id="nav"><a class="btn" href="#moreChargeNav" onclick="TR6()">View details &raquo;</a></p>
+           <p id="nav"><a class="btn" href="#moreChargeNav2" onclick="TR6()">View details &raquo;</a></p>
         </div><!-- /.span4 -->
         
       </div><!-- /.row -->
       
-      <div id="moreChargeNav" class="spaceForNav"></div>
+        <div id="moreChargeNav2" class="spaceForNav" ></div>
       
        <div  class="row">
-      	<div id="moreCharge">
+      	<div id="morechange2" class="moreCharge">
         hello
         </div>
       </div>
--- dev2/js/charge.js
+++ dev2/js/charge.js
@@ -1,49 +1,55 @@
 // JavaScript Document
 
+
 function TROnePage(){
-	GetChargeData("./charges/charge1.php");
+
+	GetChargeData("./charges/charge1.php","#morechange1","#morechange2");
 	
 }
 
 function TRLite(){
-	GetChargeData("./charges/charge2.php");
+
+	GetChargeData("./charges/charge2.php" ,"#morechange1","#morechange2");
 
 }
 
 
 function TRBespoke(){
-	GetChargeData("./charges/charge3.php");
+
+	GetChargeData("./charges/charge3.php" ,"#morechange1","#morechange2");
 
 }
 
 
 function TRStore(){
-	GetChargeData("./charges/charge4.php");
+
+	GetChargeData("./charges/charge4.php" ,"#morechange2","#morechange1");
 
 }
 
 
 function TRMobile(){
-	GetChargeData("./charges/charge5.php");
+
+	GetChargeData("./charges/charge5.php" ,"#morechange2","#morechange1");
 
 }
 
 function TR6(){
-	GetChargeData("./charges/charge6.php");
+
+	GetChargeData("./charges/charge6.php" ,"#morechange2","#morechange1");
 
 }
 
-function GetChargeData(getUrl){
-	
-	
+function GetChargeData(getUrl,divIn,divOut){
+	$(divOut).hide();
 		$.ajax({
                 url:getUrl,
                 dataType: "html",
                 success: function(data){   
-				$("#moreCharge").html(data);
+				$(divIn).html(data);
                    
                     }   
                 });
-				$("#moreCharge").css("diplay", "block");
-                $("#moreCharge").fadeIn(2000);
+				$(divIn).css("diplay", "block");
+                $(divIn).fadeIn(1000);
 	}
