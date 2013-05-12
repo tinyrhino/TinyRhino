2a02ef5530709feffe2201d5b5b36096b5700b20
Will <will@tiny-rhino.co.uk>
2013-05-12 08:34:45 +0100

    Added h1 to improve SEO

--- dev2/nav.php
+++ dev2/nav.php
@@ -6,7 +6,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <div class="logo" ><a id="logolink" href="#home"><img src="img/whitelogo.png" width="150" height="42" /></a></div>
+          <div class="logo" ><a id="logolink" href="#home"><img src="img/whitelogo.png" width="150" height="42" /><h1>Tiny Rhino</h1></a></div>
           <div class="nav-collapse collapse">
             <ul class="nav">
               <li><a href="#home">Home</a></li>
