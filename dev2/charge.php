b575a8378601788ee7d9754d4b756b46a53df230
Will <will@tiny-rhino.co.uk>
2013-05-15 18:46:39 +0100

    merged my stuff into the tinyrhino repo

--- dev2/charge.php
+++ dev2/charge.php
@@ -15,7 +15,13 @@
         <img class="img-circle chargeIcons" alt="140x140" style="width: 230px; height: 230px" src="img/onepage.png">
           <h2>One Pager</h2>
           <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
-          <p id="nav"><a class="btn" href="#moreChargeNav" onclick="TROnePage()">View details &raquo;</a></p>
+          
+          
+          <div id="more1" class="moreCharge">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed sapien risus. Curabitur mattis sagittis tempor. Duis eget arcu sem. Sed arcu nisl, hendrerit non commodo nec, placerat vel leo. Mauris ultrices, augue at consectetur lobortis, mi sapien interdum lectus, eget dignissim ligula nunc vel neque. Vivamus vel arcu tellus. Mauris adipiscing nisl hendrerit est interdum sed mollis urna varius. Quisque fringilla lectus nec turpis scelerisque pharetra.</p>
+
+          </div>
+           <a id="more1BTN" class="btn"  onclick="TRViewMore1()">View More &raquo;</a>
         </div><!-- /.span4 -->
         
         
@@ -24,7 +30,13 @@
            <img class="img-circle chargeIcons" alt="140x140" style="width: 230px; height: 230px" src="img/lite.png">
           <h2>Lite Site</h2>
           <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-           <p id="nav"><a class="btn" href="#moreChargeNav" onclick="TRLite()">View details &raquo;</a></p>
+                     
+          
+          <div id="more2" class="moreCharge">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed sapien risus. Curabitur mattis sagittis tempor. Duis eget arcu sem. Sed arcu nisl, hendrerit non commodo nec, placerat vel leo. Mauris ultrices, augue at consectetur lobortis, mi sapien interdum lectus, eget dignissim ligula nunc vel neque. Vivamus vel arcu tellus. Mauris adipiscing nisl hendrerit est interdum sed mollis urna varius. Quisque fringilla lectus nec turpis scelerisque pharetra.</p>
+         
+          </div>
+           <a id="more2BTN" class="btn"  onclick="TRViewMore2()">View More &raquo;</a>
         </div><!-- /.span4 -->
         
         
@@ -33,25 +45,32 @@
            <img class="img-circle chargeIcons" alt="140x140" style="width: 230px; height: 230px" src="img/bespoke.png">
                      <h2>Bespoke Site</h2>
           <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p id="nav"><a class="btn" href="#moreChargeNav" onclick="TRBespoke()">View details &raquo;</a></p>
+          
+          
+          <div id="more3" class="moreCharge">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed sapien risus. Curabitur mattis sagittis tempor. Duis eget arcu sem. Sed arcu nisl, hendrerit non commodo nec, placerat vel leo. Mauris ultrices, augue at consectetur lobortis, mi sapien interdum lectus, eget dignissim ligula nunc vel neque. Vivamus vel arcu tellus. Mauris adipiscing nisl hendrerit est interdum sed mollis urna varius. Quisque fringilla lectus nec turpis scelerisque pharetra.</p>
+         
+          </div>
+           <a id="more3BTN" class="btn"  onclick="TRViewMore3()">View More &raquo;</a>
         </div><!-- /.span4 -->
       </div><!-- /.row -->
       
+
       
-              <div id="moreChargeNav" class="spaceForNav" ></div>
-      
-       <div  class="row">
-      	<div id="morechange1" class="moreCharge">
-        hello
-        </div>
-      </div>
+
       
             <div  class="row">
         <div class="span4 " >
                 <img class="img-circle chargeIcons" alt="140x140" style="width: 230px; height: 230px" src="img/shopicon.png">
           <h2>Ecommerce Site</h2>
           <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
-           <p id="nav"><a class="btn" href="#moreChargeNav2" onclick="TRStore()">View details &raquo;</a></p>
+         
+          
+          <div id="more4" class="moreCharge">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed sapien risus. Curabitur mattis sagittis tempor. Duis eget arcu sem. Sed arcu nisl, hendrerit non commodo nec, placerat vel leo. Mauris ultrices, augue at consectetur lobortis, mi sapien interdum lectus, eget dignissim ligula nunc vel neque. Vivamus vel arcu tellus. Mauris adipiscing nisl hendrerit est interdum sed mollis urna varius. Quisque fringilla lectus nec turpis scelerisque pharetra.</p>
+          
+          </div>
+          <a id="more4BTN" class="btn"  onclick="TRViewMore4()">View More &raquo;</a>
         </div><!-- /.span4 -->
         
         
@@ -60,26 +79,32 @@
                 <img class="img-circle chargeIcons" alt="140x140" style="width: 230px; height: 230px" src="img/mobile.png">
           <h2>Moblie Site</h2>
           <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-           <p id="nav"><a class="btn" href="#moreChargeNav2" onclick="TRMobile()">View details &raquo;</a></p>
+          
+          
+          <div id="more5" class="moreCharge">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed sapien risus. Curabitur mattis sagittis tempor. Duis eget arcu sem. Sed arcu nisl, hendrerit non commodo nec, placerat vel leo. Mauris ultrices, augue at consectetur lobortis, mi sapien interdum lectus, eget dignissim ligula nunc vel neque. Vivamus vel arcu tellus. Mauris adipiscing nisl hendrerit est interdum sed mollis urna varius. Quisque fringilla lectus nec turpis scelerisque pharetra.</p>
+         
+          </div>
+           <a id="more5BTN" class="btn"  onclick="TRViewMore5()">View More &raquo;</a>
         </div><!-- /.span4 -->
         
         
         
         <div class="span4">
-                <img class="img-circle chargeIcons" alt="140x140" style="width: 230px; height: 230px" src="http://www.smarterwebcompany.co.uk/thesmarterwebcompany-co-uk/_img/homepage_services/seo-service.png">
+                <img class="img-circle chargeIcons" alt="140x140" style="width: 230px; height: 230px" src="img/yourlogo.png">
           <h2>Some other service because 5 is shit number</h2>
           <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-           <p id="nav"><a class="btn" href="#moreChargeNav2" onclick="TR6()">View details &raquo;</a></p>
+      
+          
+          <div id="more6" class="moreCharge">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed sapien risus. Curabitur mattis sagittis tempor. Duis eget arcu sem. Sed arcu nisl, hendrerit non commodo nec, placerat vel leo. Mauris ultrices, augue at consectetur lobortis, mi sapien interdum lectus, eget dignissim ligula nunc vel neque. Vivamus vel arcu tellus. Mauris adipiscing nisl hendrerit est interdum sed mollis urna varius. Quisque fringilla lectus nec turpis scelerisque pharetra.</p>
+          
+          </div>
+          <a id="more6BTN" class="btn"  onclick="TRViewMore6()">View More &raquo;</a>
         </div><!-- /.span4 -->
         
       </div><!-- /.row -->
       
-        <div id="moreChargeNav2" class="spaceForNav" ></div>
-      
-       <div  class="row">
-      	<div id="morechange2" class="moreCharge">
-        hello
-        </div>
-      </div>
+        
       
  </div><!-- /.featurette -->
--- dev2/js/charge.js
+++ dev2/js/charge.js
@@ -1,55 +1,96 @@
 // JavaScript Document
-
-
-function TROnePage(){
-
-	GetChargeData("./charges/charge1.php","#morechange1","#morechange2");
-	
-}
-
-function TRLite(){
-
-	GetChargeData("./charges/charge2.php" ,"#morechange1","#morechange2");
+function TRViewLess1(){
+				$('#more1BTN').html('View More');
+				$('#more1BTN').attr("onClick","TRViewMore1()");
+				$('#more1').fadeOut(1000);
 
 }
 
+function TRViewMore1(){
+				$('#more1').css("diplay", "block");
+                $('#more1').fadeIn(1000);
+				$('#more1BTN').html('View Less');
+				$('#more1BTN').attr("onClick","TRViewLess1()");
+}
 
-function TRBespoke(){
+//////////////////////////////////////////////////////////////////
 
-	GetChargeData("./charges/charge3.php" ,"#morechange1","#morechange2");
+function TRViewLess2(){
+				$('#more2BTN').html('View More');
+				$('#more2BTN').attr("onClick","TRViewMore2()");
+				$('#more2').fadeOut(1000);
 
 }
 
+function TRViewMore2(){
+				$('#more2').css("diplay", "block");
+                $('#more2').fadeIn(1000);
+				$('#more2BTN').html('View Less');
+				$('#more2BTN').attr("onClick","TRViewLess2()");
+}
 
-function TRStore(){
+//////////////////////////////////////////////////////////////////
 
-	GetChargeData("./charges/charge4.php" ,"#morechange2","#morechange1");
+function TRViewLess3(){
+				$('#more3BTN').html('View More');
+				$('#more3BTN').attr("onClick","TRViewMore3()");
+				$('#more3').fadeOut(1000);
 
 }
 
+function TRViewMore3(){
+				$('#more3').css("diplay", "block");
+                $('#more3').fadeIn(1000);
+				$('#more3BTN').html('View Less');
+				$('#more3BTN').attr("onClick","TRViewLess3()");
+}
 
-function TRMobile(){
+//////////////////////////////////////////////////////////////////
 
-	GetChargeData("./charges/charge5.php" ,"#morechange2","#morechange1");
+function TRViewLess4(){
+				$('#more4BTN').html('View More');
+				$('#more4BTN').attr("onClick","TRViewMore4()");
+				$('#more4').fadeOut(1000);
 
 }
 
-function TR6(){
+function TRViewMore4(){
+				$('#more4').css("diplay", "block");
+                $('#more4').fadeIn(1000);
+				$('#more4BTN').html('View Less');
+				$('#more4BTN').attr("onClick","TRViewLess4()");
+}
 
-	GetChargeData("./charges/charge6.php" ,"#morechange2","#morechange1");
+//////////////////////////////////////////////////////////////////
+
+function TRViewLess5(){
+				$('#more5BTN').html('View More');
+				$('#more5BTN').attr("onClick","TRViewMore5()");
+				$('#more5').fadeOut(1000);
 
 }
 
-function GetChargeData(getUrl,divIn,divOut){
-	$(divOut).hide();
-		$.ajax({
-                url:getUrl,
-                dataType: "html",
-                success: function(data){   
-				$(divIn).html(data);
-                   
-                    }   
-                });
-				$(divIn).css("diplay", "block");
-                $(divIn).fadeIn(1000);
-	}
+function TRViewMore5(){
+				$('#more5').css("diplay", "block");
+                $('#more5').fadeIn(1000);
+				$('#more5BTN').html('View Less');
+				$('#more5BTN').attr("onClick","TRViewLess5()");
+}
+
+//////////////////////////////////////////////////////////////////
+
+function TRViewLess6(){
+				$('#more6BTN').html('View More');
+				$('#more6BTN').attr("onClick","TRViewMore6()");
+				$('#more6').fadeOut(1000);
+
+}
+
+function TRViewMore6(){
+				$('#more6').css("diplay", "block");
+                $('#more6').fadeIn(1000);
+				$('#more6BTN').html('View Less');
+				$('#more6BTN').attr("onClick","TRViewLess6()");
+}
+
+//////////////////////////////////////////////////////////////////
