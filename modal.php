9de2f3236ee75316eab75213cdbdd20ce461ba7f
Will <will@tiny-rhino.co.uk>
2013-05-17 17:27:27 +0100

    added modals

--- modal.php
+++ modal.php
@@ -4,10 +4,42 @@
 <div id="privacy" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-    <h3 id="myModalLabel">Privacy</h3>
+    <h3 id="myModalLabel">Privacy Policy</h3>
   </div>
   <div class="modal-body">
-    <p>privacy</p>
+
+
+<p>Tiny Rhino respects the privacy of every individual who visits our website. Personal information will be collected only where we expressly request your personal information.&nbsp;</p>
+<p>Any personal information that we request will be used for the following purposes only:&nbsp;</p>
+
+<ol type="1" start="1">
+    <li>to fulfill any service or provide information that you might request, in which case we may disclose information to third parties solely for the purpose of fulfilling your request;&nbsp;</li>
+    
+    <li >to improve the quality of our website and the services that we provide to you;&nbsp;</li>
+    
+    <li>to send to you marketing information relating to new products or services or to notify you of changes to our website. This will only be done where you have specifically agreed to be contacted for these purposes using the "opt in" features within our website;&nbsp;</li>
+    <li >to monitor your use of our website; and&nbsp;</li>
+    <li >to aggregate statistics about customers, traffic patterns and related site information in an anonymous form.&nbsp;</li>
+</ol>
+
+<p>We will treat your personal information in accordance with the United Kingdom's Data Protection Act 1998. You have the right as an individual to find out what personal information we hold about you and to request that we correct any errors in your personal information. Also, you may request that we do not use your personal information.&nbsp;</p>
+
+<h3 >What are the benefits to me of Tiny Rhino collecting my information?&nbsp;</h3>
+
+<p>Collecting this information will help Tiny Rhino to achieve the following:<br>1. to allow Tiny Rhino to fulfill services or provide information requested by you;<br>2. to help us create website content more relevant to you;
+<br>
+3. to make improvements to our website;
+<br>
+4. to assess general trends within the website and its use; and
+<br>
+5. where you have given us your permission, to alert you to new products, special offers, updated information and other new services that Stoves considers to be of interest to you.&nbsp;</p>
+
+<h3 >How do I "opt out"?</h3>
+
+<p>We will only send marketing information to you if you have specifically requested to receive it. If you wish to opt out of the receipt of our marketing information you can select the appropriate response on the information sent to you. If you wish to withdraw your permission at any other time you may do so by contacting Tiny Rhino through the '<a href="#contactus"  data-dismiss="modal" title="Contact Us" style="outline: none;">Contact Us</a>' section of this website.&nbsp;</p>
+
+
+
   </div>
   <div class="modal-footer">
     <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
@@ -67,7 +99,7 @@
     <p>If you have any difficulties viewing our website or have suggestions for improvement, please email:&nbsp;<a title="will@tiny-rhino.co.uk" href="mailto:will@tiny-rhino.co.uk?subject=website improvements" style="outline: none;">will@tiny-rhino.co.uk</a>&nbsp;or telephone 0759 547 4098.</p>
     
 <h3>Changing the Colours and Text Size</h3>
-<p>You are able to change the colours and text size on our website. For detailed information on how to achieve this please visit the BBC's&nbsp;<a title="My Web My Way" href="http://www.bbc.co.uk/accessibility/" target="_blank" style="outline: none;">My Web My Way</a>.</p><h2 style="padding: 5px 0px 10px;">Conformance Information</h2><p>Our website conforms to HTML5 and&nbsp;CSS&nbsp;2.1. This ensures that the web pages and&nbsp;CSS&nbsp;conform to the highest specifications ensuring maximum compliance with assistive technologies and web browsers.</p>
+<p>You are able to change the colours and text size on our website. For detailed information on how to achieve this please visit the BBC's&nbsp;<a title="My Web My Way" href="http://www.bbc.co.uk/accessibility/" target="_blank" style="outline: none;">My Web My Way</a>.</p><h3>Conformance Information</h3><p>Our website conforms to HTML5 and&nbsp;CSS&nbsp;2.1. This ensures that the web pages and&nbsp;CSS&nbsp;conform to the highest specifications ensuring maximum compliance with assistive technologies and web browsers.</p>
     
   </div>
   <div class="modal-footer">
